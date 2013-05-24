23178e9efe61cc728efd7f03bcf6f352af946629
NitroGhost <wildeman.benjamin@gmail.com>
2013-05-24 02:17:28 +0200

    Update DH-room-host.js

--- DH-room-host.js
+++ DH-room-host.js
@@ -1,1 +1,5 @@
-future project
+/*
+going to make you a custom host script for your room with similar features to my room.
+Including custom colours, background (if you have one), Moderation functions and
+buttons, and the other settings found in my script
+*/
